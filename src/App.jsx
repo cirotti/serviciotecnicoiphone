33e9e17fbfa825aa23c22d5a4cfdf9284e52c2d2
cirotti <cm718199@gmail.com>
2026-06-05 16:19:54 -0400

    Landing premium iPhone final

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Metrics from "./components/Metrics";
 import Services from "./components/Services";
-import Precision from "./components/Precision";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 
